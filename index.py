89850f0037f6c1c81b4c5782b4304bd147e6e3b5
JS <Jocelynn.Sergent@ela-academy.org>
2026-03-19 15:29:00 -0400

    Save

--- index.py
+++ index.py
@@ -8,11 +8,29 @@ from MainIntro import chosen_direction
 
 chosen_direction()
 
-from MainIntro import main
-
-main()
-
 from NorthScene import north
 
 north()
+
+from SouthScene import south
+
+south()
+
+from EastScene import east
+
+east()
+
+from WestScene import west
+
+west()
+
+from OutroScene import outro
+
+outro()
+
+from StateCode import State, Game_over, Lose_HP, add_gem, has_all_gems, checkWinOrContinue
+
+State(), Game_over(), Lose_HP(), add_gem(), has_all_gems(), checkWinOrContinue()
+
+
 
